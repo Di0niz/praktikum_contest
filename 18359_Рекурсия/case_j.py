004a5482517cf16626be76a5f2049b7a0ad364cf
Denis Moskovchenko <moskovchenkody@gmail.com>
2020-07-23 00:27:07 +0300

    kondratiy

--- 18359_Рекурсия/case_j.py
+++ 18359_Рекурсия/case_j.py
@@ -1,25 +1,39 @@
 
 def grammar(n, k):
     if n == 0:
-        return '0'
+        return 0
 
-    nxt = []
-    kondr = grammar(n - 1, k)
-    print(len(kondr), hex(int(kondr, 2)))
-    for ch in kondr:
-        if ch == '0':
-            nxt.append('01')
-        elif ch == '1':
-            nxt.append('10')
-    return ''.join(nxt)
+    val = grammar(n - 1, k//2)
+
+    return val if k % 2 == 0 else ~val & 1
+
+    # nxt = []
+    # kondr = grammar(n - 1, k)
+    # for ch in kondr:
+    #     if ch == '0':
+    #         nxt.append('01')
+    #     elif ch == '1':
+    #         nxt.append('10')
+    # return ''.join(nxt)
 
 
 def main():
     n = int(input())
     k = int(input())
 
-    kondr = grammar(n, k)
-    print(len(kondr), hex(int(kondr, 2)))
+    # kond = grammar(n, k)
+    # print(kond)
+
+    # print(n, len(kondr), kondr)
+    # print(kondr[k-1])
+
+
+    # el = ''
+    # for i in range(2**n):
+    #     el += str(grammar(n, i))
+    # print('0110100110010110', '0110100110010110'[k-1])
+    # print(el, el[k-1])
+    print(grammar(n, k-1))
 
 
 def main_input():
